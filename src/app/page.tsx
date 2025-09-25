e0d9362d5a136154957caebd70974bb02bdf8b52
Nicholas <nicholassamuel34@gmail.com>
2025-09-25 11:55:16 +0700

    fix: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,8 +33,8 @@ export default function Home() {
     <h1 className="text-3xl bg-sky-500 font-bold underline">
       Hello world!
     </h1>
-    <h1 className="text-3xl bg-reeva-secondary-5 font-bold underline">haloo</h1>
-    <div className="w-10 h-10 bg-slate-700"></div>
+    {/* <h1 className="text-3xl bg-reeva-secondary-5 font-bold underline">haloo</h1>
+    <div className="w-10 h-10 bg-slate-700"></div> */}
     <Button variant="blue" size="lg">
       Button
     </Button>
